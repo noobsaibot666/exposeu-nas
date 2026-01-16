4c35492e9140eec7ea4f493ffa4d7c6e1e58337e
Alan Alves <your-email@example.com>
2026-01-16 17:03:42 +0100

    Gallery Stories page

--- src/pages/GalleryStories.tsx
+++ src/pages/GalleryStories.tsx
@@ -2,9 +2,9 @@ import { resolveImagePath } from '../utils/resolveImagePath'
 import WorkPageLayout from './WorkPageLayout'
 
 const heroCards = [
-  { image: resolveImagePath('/src/assets/images/services/2_gallery_work/2_GW_010.png'), title: 'Curator Voice' },
+  { image: resolveImagePath('/src/assets/images/services/2_gallery_work/_thumb/9_16/2_GW_024.png'), title: 'Curator Voice' },
   { image: resolveImagePath('/src/assets/images/services/2_gallery_work/2_GW_013.png'), title: 'Install Details' },
-  { image: resolveImagePath('/src/assets/images/services/2_gallery_work/2_GW_020.png'), title: 'Space Flow' },
+  { image: resolveImagePath('/src/assets/images/services/2_gallery_work/_thumb/9_16/7_HERO_015.png'), title: 'Space FLow' },
   { image: resolveImagePath('/src/assets/images/services/2_gallery_work/2_GW_025.png'), title: 'Patron Moments' },
 ]
 
